afa3191833aecfc0a1f965c9387cf256848777bf
Christopher Carrick <christopher@weare8.com>
2020-09-12 01:29:09 -0400

    Remove duplicate database connection

--- server/data/index.js
+++ server/data/index.js
@@ -1,10 +1,3 @@
-import mongoose from 'mongoose';
-
-mongoose.connect(process.env.DATABASE_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
 export * from './channel';
 export * from './message';
 export * from './server';
